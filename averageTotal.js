4932c773120fca37bfc444959f2b9119d424ea56
Maisnam Raju Singh <maisnamraju@gmail.com>
2017-03-25 02:17:01 +0530

    fixed issue with undefined values

--- averageTotal.js
+++ averageTotal.js
@@ -21,21 +21,22 @@ function mapFunction(){
 	let hour = this.hour;
 	let total = 0;
 	let average = 0;
-	for(let minute in this.minutesSecondsData){
-		for(let second in minute){
-			total += this.minutesSecondsData[minute][second];
+	 for(let minute in this.minutesSecondsData){
+ 		for(let second in this.minutesSecondsData[minute]){
+ 			total += this.minutesSecondsData[minute][second];
 		}
-	}
-	average = total/360;
-    let date = this.hour.setUTCHours(0);	
-	emit(this.hour,{ total: total, average: average });
+
+ 	}
+	 average = total/360;
+     let date = this.hour.setUTCHours(0);
+	 emit(this.hour,{ total: total, average: average });
 }
 
 function reduceFunction(key,values){
 	let final = { total: 0, average: 0 }
 	for(let item in values ){
-	   final.total += item.total;
-	   final.average +=item.average;
+	  final.total += item.total;
+	  final.average +=item.average;
 	}
 	final.average = final.average/values.length;
 	return final;
